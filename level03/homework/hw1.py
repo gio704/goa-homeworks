a02a860f6006cc65ba3c25676290117af03266bc
gio704 <gpatashuri26@gmail.com>
2025-02-25 18:50:04 +0400

    Add files via upload

---
+++ level03/homework/hw1.py
@@ -0,0 +1,8 @@
+age = int(input("შეიყვანეთ თქვენი ასაკი: "))
+
+if age <= 2:
+    print("ბაღი")
+elif age >= 6:
+    print("სკოლა")
+else:
+    print("სამსახური")
---
+++ level04/homework/hw3.py
@@ -0,0 +1,14 @@
+score = int(input("enter your score: "))
+
+if score > 90:
+    print("score: A")
+elif score > 75:
+    print("score: B")
+elif score > 60:
+    print("score: C")
+elif score > 50:
+    print("score: D")
+elif score > 40:
+    print("score: E")
+else:
+    print("score: F")
