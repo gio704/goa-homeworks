ff92e4053910266f99d1c94d6e6b9ee0451c932e
gio704 <gpatashuri26@gmail.com>
2025-02-26 20:46:11 +0400

    Add files via upload

---
+++ level05/homework/hw4.py
@@ -0,0 +1,12 @@
+
+user_word = input("Enter word: ") 
+for word in user_word:
+    if word != "A":
+         print(" ")
+     elif word == ("A"):
+         print(word)
+     else:
+         pass
+
+
+
