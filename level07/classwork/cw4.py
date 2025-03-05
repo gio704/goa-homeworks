6f17a30887a367280a073ee1a494bbfa4f828ae0
gio704 <gpatashuri26@gmail.com>
2025-03-05 23:01:09 +0400

    Delete level07 directory

--- level07/classwork/cw4.py
+++
@@ -1,2 +0,0 @@
-for i in range(250, 501, 10):
-    print(i)
--- level07/classwork/cw6.py
+++
@@ -1,7 +0,0 @@
-number = int(input("enter a number: "))
-
-if number % 2 == 0:
-    for _ in range(10):
-        print("yes")
-else:
-    print("No")
