d83fbe752f2bbf7e5452e16908c39a36f43b3ccf
gio704 <gpatashuri26@gmail.com>
2025-03-10 15:40:17 +0400

    Add files via upload

---
+++ level08/classwork/cw3.py
@@ -0,0 +1,10 @@
+  user_input = input("enter a word with 3 letter")
+
+    if len(user_input) != 3:
+        print("enter a word with 3 letter")
+    else:
+        is_palindrome = True
+        for i in range(len(user_input) // 2):
+            if user_input[i] != user_input[-(i + 1)]:
+                is_palindrome = False
+        print(is_palindrome)
