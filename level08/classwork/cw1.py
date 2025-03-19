d1a9cc9c20f21007029a15cc0fad8674baa3ea55
gio704 <gpatashuri26@gmail.com>
2025-03-19 15:51:08 +0400

    Delete level08 directory

--- level08/classwork/cw1.py
+++
@@ -1,9 +0,0 @@
-reversed_string = input("enter a word: ")
-res = " "
-for i in reversed_string:
-    res = i + res
-
-print(res)
-
-
-
--- level08/classwork/cw2.py
+++
@@ -1,5 +0,0 @@
-sum = 0
-for number in range(101):
-    sum += number
-
-print("correct number:", sum)
