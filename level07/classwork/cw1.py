e0ab76b1929b17a6f0b9b89bd3dd01d51b6eac3d
gio704 <gpatashuri26@gmail.com>
2025-03-19 15:55:00 +0400

    Add files via upload

---
+++ level07/classwork/cw1.py
@@ -0,0 +1,5 @@
+text = str(input("enter a text: "))
+num = int(input("enter a number: "))
+print(text[num])
+
+
---
+++ level07/classwork/cw5.py
@@ -0,0 +1,6 @@
+word = input("please enter a word: ")
+
+if word[0] == '_':
+    print(True)
+else:
+    print(False)
